4d1e022b4b0ae1dad59714b7bdd2efe78f96f74d
Roman Oknyansky <Roman.Oknyansky@ecitele.com>
2019-07-22 14:57:55 +0300

    added sleeps

--- run-docker.sh
+++ run-docker.sh
@@ -3,9 +3,9 @@
 docker pull warrior7089/rabbitmq:1.0
 docker swarm init
 docker stack deploy -c docker-compose.yml rab
-sleep 10
+sleep 30
 docker exec -i $(docker ps -q -f 'name=rab_rabbitmq') bash -c 'echo -e $RABBITMQ_ERLANG_COOKIE > /var/lib/rabbitmq/.erlang.cookie'
 docker exec -i $(docker ps -q -f 'name=rab_rabbitmq') bash -c 'chmod 600 /var/lib/rabbitmq/.erlang.cookie'
 docker exec -i $(docker ps -q -f 'name=rab_rabbitmq') pkill sleep
-sleep 5
+sleep 10
 docker exec -i $(docker ps -q -f 'name=rab_rabbitmq') rabbitmq-plugins enable rabbitmq_management
